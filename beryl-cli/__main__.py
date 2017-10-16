b157d76680bd2b1d0bce9f0d36733bae7430433e
Justin Payne <justin.payne@fda.hhs.gov>
2017-10-16 16:45:04 -0400

    work

--- beryl-cli/__main__.py
+++ beryl-cli/__main__.py
@@ -4,27 +4,30 @@
 Beryl - distributed bioinformatics resource management system
 
 Usage:
+    beryl login [--logout]
     beryl register <name> <path>...
-    beryl update <name> <update_key> <path>...
+    beryl update <name> <path>...
     beryl fork <name>
     beryl pull <name>
     beryl list
+	beryl del <name>...
     beryl version
     beryl help
-    beryl <name>
+    beryl <name> [<commit>]
 
 """
 
 version = '0.1a'
 
+import capnp
 import lzma
 from docpie import docpie
 from halo import Halo
 from sys import argv
-from protocol.beryl_capnp import Register, Update, Fork, Pull, List, Info, Version, Response
+from protocol.beryl_capnp import Server
 import os
 
-host = os.environ.get(BERYL_SOCKET, '')
+host = os.environ.get('BERYL_SOCKET', '')
 
 dispatch = {}
 
@@ -42,37 +45,61 @@ def main():
 		return resolve(**cmd)
 
 #this is a command but not a keyword
-def resolve(name, client=None, *a, **k):
+@command('')
+def resolve(name, commit=None, client=None, *a, **k):
+	"resolve a local path to a database; typically used with subshell expansion"
 	print(client.resolve(name=name).wait().path)
+
+@command('login')
+def login(client=None, *a, **k):
+	"login to the IPFS swarm"
+	pass
 
 @command('version')
 def version(client=None, *a, **k):
+	"""print this help and exit."""
 	print("beryl-client v. {}".format(version))
 	print("beryl-daemon v. {}".format(client.version().wait().version))
 
 @command('register')
 def register(name, path=[], client=None, *a, **k):
-	archive = lzma.LZMACompressor(check=lzma.CHECK_SHA256)
-	if not path:
-		path = (os.cwd(), )
-	if os.path.isdir(path):
-		pass
+	"create a database under a findable name"
+	with Halo(text='Compressing...', spinner='line'):
+		archive = lzma.LZMACompressor(check=lzma.CHECK_SHA256)
+		if not path:
+			path = (os.cwd(), )
+		if os.path.isdir(path):
+			pass
+
+@command('del')
+def delete(name, client=None, *a, **k):
+	"remove a database from your local storage"
+	pass
 
 @command('update')
 def update(name, path=[], client=None, *a, **k):
-	pass
+	"update an existing database"
+	with Halo(text='Compressing...', spinner='line'):
+		pass
 
 @command('fork')
 def fork(name, client=None, *a, **k):
+	"fork a database into your namespace"
 	pass
 
 @command('pull')
 def pull(name, client=None, *a, **k):
-	pass
+	"pull a database from the IPFS swarm"
+	with Halo(text='Downloading...', spinner='line'):
 
 @command('list')
 def list_f(name, client=None, *a, **k):
-	pass
+	"list locally-stored databases"
+	with Halo(text='Contacting...', spinner='line'):
+		response = client.list().wait()
+	print('header here')
+	for rec in client.list().wait():
+		print("{rec}".format(rec=rec))
 
 
 	
--- beryl/beryl.py
+++ beryl/beryl.py
@@ -1,1 +1,33 @@
-from protocol.classes.python import *
+#!/usr/env python3
+
+import capnp
+import ipfsapi
+from protocol.beryl_capnp import Server, Version, Resolution, ActionResult, PollingResult, Listing
+
+version = '0.1a'
+
+class TestServerImpl(Server):
+	
+	def version(self) -> Version:
+		pass
+
+	def resolve(self, name) -> Resolution:
+		pass
+
+	def register(self, name, bytes) -> PollingResult:
+		pass
+
+	def update(self, name, bytes) -> PollingResult:
+		pass
+
+	def fork(self, name) -> str:
+		pass
+
+	def pull(self, name) -> PollingResult:
+		pass
+
+	def list(self) -> list:
+		pass
+
+class ActualServerImpl(Server):
+	pass
